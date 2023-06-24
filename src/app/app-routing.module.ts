5df67c1cd0f3522ee66ba5abeed64f8f56b8344a
Erkan Yıldırım <erkanyildirim28@gmail.com>
2023-06-24 15:09:32 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,52 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NavigationError, NavigationStart, Router, RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./guest/home/home.component";
+import {LoginComponent} from "./guest/login/login.component";
+import {RegisterComponent} from "./guest/register/register.component";
+import {ProfileComponent} from "./user/profile/profile.component";
+import {AdminComponent} from "./admin/admin/admin.component";
+import {NotFoundComponent} from "./error/not-found/not-found.component";
+import {UnauthorizedComponent} from "./error/unauthorized/unauthorized.component";
+import {filter} from "rxjs";
+import {Role} from "../models/role.enum";
+import {AuthClassGuard} from "./guards/auth-class.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"home",pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+
+  {
+    path:'profile',
+    component:ProfileComponent,
+    canActivate:[AuthClassGuard],
+    data:{roles:[Role.ADMIN,Role.USER]}
+  },
+
+  {
+    path:'admin',
+    component:AdminComponent,
+    canActivate:[AuthClassGuard],
+    data:{roles:[Role.ADMIN]}
+  },
+
+  {path:'404',component:NotFoundComponent},
+  {path:'401',component:UnauthorizedComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationError))
+      .subscribe(() =>
+        // Handling NavigationError, e.g. redirect to error route
+        this.router.navigate(['/404'], { skipLocationChange: true })
+      );
+  }
+
+}
---
+++ src/app/guards/auth-class.guard.ts
@@ -0,0 +1,35 @@
+import {inject, Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthenticationService} from "../services/authentication.service";
+import {User} from "../../models/user.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthClassGuard implements CanActivate {
+
+  private currentUser:User= new User;
+
+  constructor(private authenticationService:AuthenticationService,private router:Router) {
+     this.authenticationService.currentUser.subscribe(data=>{
+        this.currentUser=data;
+     })
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if(this.currentUser){
+      if(route.data.roles?.indexOf(this.currentUser.role)===-1){
+        this.router.navigate(['/401']);
+        return false;
+      }
+
+      return true;
+    }
+
+    this.router.navigate(['/login']);
+    return true;
+  }
+}
